cdfa642148b3317ce19e6f97a78d8823032eb543
LeaLem13 <lea.lemans@uliege.be>
2026-02-05 09:50:15 +0100

    léa

--- scripts/OGF_typologie_2.R
+++ scripts/OGF_typologie_2.R
@@ -1,8 +1,7 @@
 # =========================================================
-# OGF_all.db : essmaj "mature" (gros arbres) + typologie_mature
-#            + plus gros arbre (CIR_max, ess_max)
-#            + EXCLUSION UE si présence de CR/MZ/DO -> NA + flag
-#            + écriture dans dendro_plot (SQLite)
+# OGF_all.db : AJOUT typologie_mature_simplifiee (SAFE UPDATE par clés)
+# - ne modifie PAS le nombre de lignes de dendro_plot
+# - update par (ues_id_ogf, ues_id_ue), pas par rowid
 # =========================================================
 
 library(DBI)
@@ -15,22 +14,22 @@ chemin_bd <- "C:/Users/Lemans Léa/Documents/GitHub/OldGrowthForest/data/OGF_all
 # ----------------------------
 # 1) Seuils
 # ----------------------------
-seuil_pur   <- 66.7
+seuil_pur   <- 66.7  # IPRFW
 seuil_autre <- 50
+seuil_dom   <- 50
 
 # ----------------------------
-# 2) Codes (regroupements)
+# 2) Codes
 # ----------------------------
 codes_chene  <- c("CH","CP","CS","CHs")
 codes_erable <- c("ER","EP","ES")
 
 codes_nobles <- c("HE", codes_chene, "FR", codes_erable, "MR", "CR")
-
 codes_feuillus <- c("HE", codes_chene, "FR", codes_erable, "MR", "CR",
                     "CA","AUs","BOU","BOs","TIs","PPN","PG")
 
 # ----------------------------
-# 3) Parser essmaj 
+# 3) Parser essmaj
 # ----------------------------
 parser_essmaj <- function(x) {
   x <- ifelse(is.na(x), "", x)
@@ -51,7 +50,7 @@ parser_essmaj <- function(x) {
 }
 
 # ----------------------------
-# 4) Typologie
+# 4) Typologie mature (ancienne) - inchangée
 # ----------------------------
 typologie_une_ligne <- function(code1, p1, code2, p2) {
   code1 <- str_trim(ifelse(is.na(code1), "", code1))
@@ -59,7 +58,6 @@ typologie_une_ligne <- function(code1, p1, code2, p2) {
   p1 <- ifelse(is.na(p1), 0, p1)
   p2 <- ifelse(is.na(p2), 0, p2)
   
-  # règle spéciale : dominante = érable (sans seuil)
   if (code1 %in% codes_erable) return("Erabliere")
   
   total_chene  <- ifelse(code1 %in% codes_chene, p1, 0) + ifelse(code2 %in% codes_chene, p2, 0)
@@ -69,7 +67,6 @@ typologie_une_ligne <- function(code1, p1, code2, p2) {
   total_mr     <- ifelse(code1=="MR", p1, 0) + ifelse(code2=="MR", p2, 0)
   total_cr     <- ifelse(code1=="CR", p1, 0) + ifelse(code2=="CR", p2, 0)
   
-  # types purs
   if (total_chene  >= seuil_pur) return("Chenaie")
   if (total_he     >= seuil_pur) return("Hetraie")
   if (total_fr     >= seuil_pur) return("Frenaie")
@@ -77,33 +74,65 @@ typologie_une_ligne <- function(code1, p1, code2, p2) {
   if (total_mr     >= seuil_pur) return("Merisier")
   if (total_cr     >= seuil_pur) return("Chene rouge")
   
-  # mélanges
   if ((total_he + total_chene) >= seuil_pur && total_he < seuil_pur && total_chene < seuil_pur) return("Hetre - Chene")
   if ((total_fr + total_chene) >= seuil_pur && total_fr < seuil_pur && total_chene < seuil_pur) return("Chene - Frene")
   
-  # autres nobles (proxy top2)
   nobles_visibles <- ifelse(code1 %in% codes_nobles, p1, 0) + ifelse(code2 %in% codes_nobles, p2, 0)
   if (nobles_visibles >= seuil_pur) return("Autres feuillus nobles")
   
-  # autres feuillus (proxy top2)
   feuillus_visibles <- ifelse(code1 %in% codes_feuillus, p1, 0) + ifelse(code2 %in% codes_feuillus, p2, 0)
   if (feuillus_visibles >= seuil_autre) return("Autres peuplements feuillus")
   
   "Peuplement melange"
 }
 
+# ----------------------------
+# 4bis) Typologie mature simplifiée (nouvelle)
+# ----------------------------
+typologie_mature_simplifiee_une_ligne <- function(code1, p1, code2, p2) {
+  code1 <- str_trim(ifelse(is.na(code1), "", code1))
+  code2 <- str_trim(ifelse(is.na(code2), "", code2))
+  p1 <- ifelse(is.na(p1), 0, p1)
+  p2 <- ifelse(is.na(p2), 0, p2)
+  
+  nobles_visibles <- ifelse(code1 %in% codes_nobles, p1, 0) + ifelse(code2 %in% codes_nobles, p2, 0)
+  feuillus_visibles <- ifelse(code1 %in% codes_feuillus, p1, 0) + ifelse(code2 %in% codes_feuillus, p2, 0)
+  
+  if (p1 >= seuil_dom) {
+    if (code1 == "HE")            return("Hetraie")
+    if (code1 %in% codes_chene)   return("Chenaie")
+    if (code1 == "FR")            return("Frenaie")
+    if (code1 %in% codes_erable)  return("Erabliere")
+    
+    if (code1 %in% codes_nobles)   return("Autres feuillus nobles")
+    if (code1 %in% codes_feuillus) return("Autres peuplements feuillus")
+    return("Peuplement melange")
+  }
+  
+  if (nobles_visibles >= seuil_pur)     return("Autres feuillus nobles")
+  if (feuillus_visibles >= seuil_autre) return("Autres peuplements feuillus")
+  
+  "Peuplement melange"
+}
+
 # =========================================================
-# 5) Connexion DB + lecture tables
+# 5) Connexion + lecture
 # =========================================================
 con <- dbConnect(RSQLite::SQLite(), chemin_bd)
 
-dendro_plot <- dbReadTable(con, "dendro_plot")
+dendro_plot_db <- dbReadTable(con, "dendro_plot")
 arbre <- dbReadTable(con, "arbre")
 
 key <- c("ues_id_ogf", "ues_id_ue")
 
+# --- sécurité : clé unique dans dendro_plot ---
+dup_key <- dendro_plot_db %>% count(across(all_of(key))) %>% filter(n > 1)
+if (nrow(dup_key) > 0) {
+  stop("dendro_plot contient des doublons sur (ues_id_ogf, ues_id_ue). Corrige avant update.")
+}
+
 # =========================================================
-# 6) ESSMAJ "MATURE" : top 40 tiges/ha les plus grosses (vivantes)
+# 6) ESSMAJ mature (top 40 tiges/ha) + exclusions
 # =========================================================
 n_tree_essMatureMaj <- 40
 
@@ -132,9 +161,6 @@ trees_top <- arbre %>%
   filter(gha_dom > 0) %>%
   ungroup()
 
-# =========================================================
-# 6bis) EXCLUSION UE si présence de CR/MZ/DO dans trees_top (on mettra NA + flag)
-# =========================================================
 codes_exclure <- c("CR","MZ","DO")
 
 ue_exclues <- trees_top %>%
@@ -144,9 +170,6 @@ ue_exclues <- trees_top %>%
 trees_top_ok <- trees_top %>%
   anti_join(ue_exclues, by = key)
 
-# =========================================================
-# 6ter) Calcul essmaj_mature sur UE non exclues
-# =========================================================
 gha_ess_mature <- trees_top_ok %>%
   group_by(ues_id_ogf, ues_id_ue, ess) %>%
   summarise(gha_ess = sum(gha_dom, na.rm = TRUE), .groups = "drop")
@@ -172,90 +195,91 @@ essmaj_mature <- gha_ess_mature %>%
   )
 
 # =========================================================
-# 7) Construire dendro_plot2 proprement + flag valid_mature + NA exclues
-# =========================================================
-dendro_plot2 <- dendro_plot %>%
-  select(-any_of(c("essmaj_mature","typologie_mature","valid_mature","CIR_max","ess_max"))) %>%
-  left_join(essmaj_mature, by = key) %>%
-  mutate(valid_mature = 1L) %>%
-  left_join(ue_exclues %>% mutate(exclu = 1L), by = key) %>%
-  mutate(
-    valid_mature = ifelse(!is.na(exclu), 0L, valid_mature),
-    essmaj_mature = ifelse(valid_mature == 0L, NA_character_, essmaj_mature)
-  ) %>%
-  select(-exclu)
-
-# =========================================================
-# 8) Typologie sur essmaj_mature
-# =========================================================
-parsed_m <- parser_essmaj(dendro_plot2$essmaj_mature)
-
-dendro_plot2$typologie_mature <- mapply(
-  typologie_une_ligne,
-  parsed_m$code1, parsed_m$p1, parsed_m$code2, parsed_m$p2
-)
-
-dendro_plot2$typologie_mature[is.na(dendro_plot2$essmaj_mature) | dendro_plot2$essmaj_mature==""] <- NA
-
-# =========================================================
-# 9) Plus gros arbre (vivant) par UE : CIR_max + ess_max
+# 7) Plus gros arbre (vivant) par UE : CIR_max + ess_max
 # =========================================================
 max_tree <- arbre %>%
   filter(statut == 1, !is.na(circ), circ > 0, !is.na(ess), ess != "") %>%
   group_by(ues_id_ogf, ues_id_ue) %>%
   slice_max(order_by = circ, n = 1, with_ties = FALSE) %>%
-  transmute(
-    ues_id_ogf, ues_id_ue,
-    CIR_max = circ,
-    ess_max = ess
-  ) %>%
+  transmute(ues_id_ogf, ues_id_ue, CIR_max = circ, ess_max = ess) %>%
   ungroup()
 
-dendro_plot2 <- dendro_plot2 %>%
-  left_join(max_tree, by = key)
-
 # =========================================================
-# 10) Écriture dans SQLite (ALTER + UPDATE via rowid)
+# 8) Construire une table UPDATE (1 ligne par UE, clés uniques)
 # =========================================================
-cols <- dbGetQuery(con, "PRAGMA table_info(dendro_plot);")$name
-if (!"essmaj_mature" %in% cols)    dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN essmaj_mature TEXT;")
-if (!"typologie_mature" %in% cols) dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN typologie_mature TEXT;")
-if (!"valid_mature" %in% cols)     dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN valid_mature INTEGER;")
-if (!"CIR_max" %in% cols)          dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN CIR_max REAL;")
-if (!"ess_max" %in% cols)          dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN ess_max TEXT;")
+update_tbl <- dendro_plot_db %>%
+  select(all_of(key)) %>%
+  left_join(essmaj_mature, by = key) %>%
+  left_join(max_tree, by = key) %>%
+  left_join(ue_exclues %>% mutate(exclu = 1L), by = key) %>%
+  mutate(
+    valid_mature = ifelse(is.na(exclu), 1L, 0L),
+    essmaj_mature = ifelse(valid_mature == 0L, NA_character_, essmaj_mature)
+  ) %>%
+  select(-exclu)
 
-rowids <- dbGetQuery(con, "SELECT rowid FROM dendro_plot;")
-stopifnot(nrow(rowids) == nrow(dendro_plot2))
+# typologies sur essmaj_mature
+parsed_m <- parser_essmaj(update_tbl$essmaj_mature)
 
-tmp <- data.frame(
-  rowid = rowids$rowid,
-  essmaj_mature = dendro_plot2$essmaj_mature,
-  typologie_mature = dendro_plot2$typologie_mature,
-  valid_mature = dendro_plot2$valid_mature,
-  CIR_max = dendro_plot2$CIR_max,
-  ess_max = dendro_plot2$ess_max
+update_tbl$typologie_mature <- mapply(
+  typologie_une_ligne,
+  parsed_m$code1, parsed_m$p1, parsed_m$code2, parsed_m$p2
 )
 
-dbWriteTable(con, "tmp_typo_mature", tmp, overwrite = TRUE)
+update_tbl$typologie_mature_simplifiee <- mapply(
+  typologie_mature_simplifiee_une_ligne,
+  parsed_m$code1, parsed_m$p1, parsed_m$code2, parsed_m$p2
+)
+
+wh_na <- is.na(update_tbl$essmaj_mature) | update_tbl$essmaj_mature == ""
+update_tbl$typologie_mature[wh_na] <- NA
+update_tbl$typologie_mature_simplifiee[wh_na] <- NA
+
+# =========================================================
+# 9) ALTER TABLE si besoin + UPDATE SQL via table temporaire (par clés)
+# =========================================================
+cols <- dbGetQuery(con, "PRAGMA table_info(dendro_plot);")$name
+if (!"essmaj_mature" %in% cols)                 dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN essmaj_mature TEXT;")
+if (!"typologie_mature" %in% cols)              dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN typologie_mature TEXT;")
+if (!"typologie_mature_simplifiee" %in% cols)   dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN typologie_mature_simplifiee TEXT;")
+if (!"valid_mature" %in% cols)                  dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN valid_mature INTEGER;")
+if (!"CIR_max" %in% cols)                       dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN CIR_max REAL;")
+if (!"ess_max" %in% cols)                       dbExecute(con, "ALTER TABLE dendro_plot ADD COLUMN ess_max TEXT;")
+
+dbWriteTable(con, "tmp_update_mature", update_tbl, overwrite = TRUE)
 
 dbExecute(con, "
   UPDATE dendro_plot
   SET
-    essmaj_mature = (SELECT essmaj_mature FROM tmp_typo_mature WHERE tmp_typo_mature.rowid = dendro_plot.rowid),
-    typologie_mature = (SELECT typologie_mature FROM tmp_typo_mature WHERE tmp_typo_mature.rowid = dendro_plot.rowid),
-    valid_mature = (SELECT valid_mature FROM tmp_typo_mature WHERE tmp_typo_mature.rowid = dendro_plot.rowid),
-    CIR_max = (SELECT CIR_max FROM tmp_typo_mature WHERE tmp_typo_mature.rowid = dendro_plot.rowid),
-    ess_max = (SELECT ess_max FROM tmp_typo_mature WHERE tmp_typo_mature.rowid = dendro_plot.rowid);
+    essmaj_mature = (SELECT essmaj_mature FROM tmp_update_mature
+                     WHERE tmp_update_mature.ues_id_ogf = dendro_plot.ues_id_ogf
+                       AND tmp_update_mature.ues_id_ue  = dendro_plot.ues_id_ue),
+    typologie_mature = (SELECT typologie_mature FROM tmp_update_mature
+                        WHERE tmp_update_mature.ues_id_ogf = dendro_plot.ues_id_ogf
+                          AND tmp_update_mature.ues_id_ue  = dendro_plot.ues_id_ue),
+    typologie_mature_simplifiee = (SELECT typologie_mature_simplifiee FROM tmp_update_mature
+                                   WHERE tmp_update_mature.ues_id_ogf = dendro_plot.ues_id_ogf
+                                     AND tmp_update_mature.ues_id_ue  = dendro_plot.ues_id_ue),
+    valid_mature = (SELECT valid_mature FROM tmp_update_mature
+                    WHERE tmp_update_mature.ues_id_ogf = dendro_plot.ues_id_ogf
+                      AND tmp_update_mature.ues_id_ue  = dendro_plot.ues_id_ue),
+    CIR_max = (SELECT CIR_max FROM tmp_update_mature
+               WHERE tmp_update_mature.ues_id_ogf = dendro_plot.ues_id_ogf
+                 AND tmp_update_mature.ues_id_ue  = dendro_plot.ues_id_ue),
+    ess_max = (SELECT ess_max FROM tmp_update_mature
+               WHERE tmp_update_mature.ues_id_ogf = dendro_plot.ues_id_ogf
+                 AND tmp_update_mature.ues_id_ue  = dendro_plot.ues_id_ue);
 ")
 
-dbExecute(con, "DROP TABLE tmp_typo_mature;")
+dbExecute(con, "DROP TABLE tmp_update_mature;")
 dbDisconnect(con)
 
 # =========================================================
-# 11) Contrôles rapides
+# 10) Contrôles rapides (sans relire la DB, on check update_tbl)
 # =========================================================
-dendro_plot2 %>% count(valid_mature, sort = TRUE)
-dendro_plot2 %>% count(typologie_mature, sort = TRUE)
-dendro_plot2 %>% count(ess_max, sort = TRUE)
+update_tbl %>% count(valid_mature, sort = TRUE)
+update_tbl %>% count(typologie_mature, sort = TRUE)
+update_tbl %>% count(typologie_mature_simplifiee, sort = TRUE)
+update_tbl %>% count(ess_max, sort = TRUE)
 
-head(dendro_plot2[, c("ues_id_ogf","ues_id_ue","valid_mature","essmaj_mature","typologie_mature","CIR_max","ess_max")])
+head(update_tbl)
